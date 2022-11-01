2769b61c5fb6a2077b0b8ddcc3f57c53c1215c7e
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-01 11:43:16 -0400

    commit tree 9727e6c4cc267c97b65ad9de743a36e89b14003d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "bad"
+function upperCaseCustomerName(){ 
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() { 
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() { 
+    bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'moge'; 
+} 
